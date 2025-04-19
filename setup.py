95ddacc062695490b98a8b98b743c7070c61ffb2
Tal Wrii <talwrii@gmail.com>
2025-04-19 03:39:16 +0200

    release 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='json-xwininfo',
-    version="1.0.0",
+    version="1.1.0",
     author='@readwithai',
     long_description_content_type='text/markdown',
     author_email='talwrii@gmail.com',
